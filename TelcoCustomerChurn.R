1be02f4862b8c86f697221d1243b79c1f7cecf30
Nas-virat <Napasvinitnantharat@gmail.com>
2022-04-16 13:05:27 +0700

    3 Question telco Napas

--- TelcoCustomerChurn.R
+++ TelcoCustomerChurn.R
@@ -16,6 +16,26 @@ ggplot(data = df) +
   geom_bar(mapping = aes(x=Churn,fill = Churn)) +
   scale_fill_brewer(palette="Dark2")
 
+#Distribution of tenure and Churn
+# Is the longer tenure make less Churn 
+ggplot(df,aes(x=tenure,fill = Churn))+
+  geom_histogram(binwidth = 2 ,alpha=0.5, position="identity")+
+  ggtitle("Distribution of tenure and Churn")+
+  xlab("tenure (months)")+
+  ylab("Number of Customer")
+
+
+#Distrubution of TotalCharges and Churn
+# Is TotalCharges tell us about the Churn of each customer
+ggplot(df,aes(x=TotalCharges,fill = Churn))+
+  geom_histogram(binwidth = 100 ,alpha=0.6, position="identity")+
+  ggtitle("Distribution of TotalCharges and Churn")+
+  xlab("TotalCharges (Dollar)")+
+  ylab("Number of Customer")
+
+# Is paymentMethod tell us about the Churn of each customer
+
+
 
 
 #list column name 
